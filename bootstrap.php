c7af62e74311fbd258528d90dcedf6afb55854f8
Dmitry Vasilyuk <vasilukwolf@gmail.com>
2025-04-04 19:28:41 +0400

    Inital commit

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+$configuration = array(
+    'db_dsn'  => 'mysql:host=127.127.126.50;dbname=oo_battle',
+    'db_user' => 'wolf',
+    'db_pass' => 'pass1234',
+);
+
+require_once __DIR__.'/lib/Service/Container.php';
+require_once __DIR__.'/lib/Model/Ship.php';
+require_once __DIR__.'/lib/Service/BattleManager.php';
+require_once __DIR__.'/lib/Service/ShipLoader.php';
+require_once __DIR__.'/lib/Model/BattleResult.php';
