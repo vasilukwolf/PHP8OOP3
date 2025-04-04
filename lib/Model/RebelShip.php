c615a8af2316fa9634debbba38cb18fca303ec83
Dmitry Vasilyuk <vasilukwolf@gmail.com>
2025-04-04 21:43:43 +0400

    Protected Visibility

--- lib/Model/RebelShip.php
+++ lib/Model/RebelShip.php
@@ -19,4 +19,24 @@ class RebelShip extends Ship
         return true;
     }
 
+    public function getNameAndSpecs($useShortFormat = false)
+    {
+        if ($useShortFormat) {
+            return sprintf(
+                '%s: %s/%s/%s (Rebel)',
+                $this->getName(),
+                $this->getWeaponPower(),
+                $this->getJediFactor(),
+                $this->getStrength()
+            );
+        } else {
+            return sprintf(
+                '%s: w:%s, j:%s, s:%s (Rebel)',
+                $this->getName(),
+                $this->getWeaponPower(),
+                $this->getJediFactor(),
+                $this->getStrength()
+            );
+        }
+    }
 }
